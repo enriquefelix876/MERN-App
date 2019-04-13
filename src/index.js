37a7dc752ddddc9cde6658059b5db0e49babbd07
Enrique Felix <enriquefelix876@gmail.com>
2019-04-12 18:45:50 -0700

    nodemon -D morgan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Servidor iniciado");
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+
+//Routes
+
+//Static files
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log("Servidor iniciado en el puerto "+app.get('port'));
 });
